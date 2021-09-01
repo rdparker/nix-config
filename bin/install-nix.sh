b90800f9666d0f35bbcec38c5488b93423c4c126
Ron Parker <rdparker@gmail.com>
2021-09-01 16:07:25 -0400

    Add nix-darwin to the install script

--- bin/install-nix.sh
+++ bin/install-nix.sh
@@ -12,3 +12,21 @@ if command -v nix > /dev/null; then
 else
     bash <(curl -L $URL) --daemon $FLAG
 fi
+
+if [[ $(uname -s) = "Darwin" ]]; then
+    BINDIR=$(dirname $0)
+    if nix-channel --list | grep -qw ^darwin > /dev/null; then
+        echo "The nix-darwin channel is already installed on this system."
+    else
+        nix-channel --add https://github.com/LnL7/nix-darwin/archive/master.tar.gz darwin
+        nix-channel --update
+    fi
+
+    if command -v darwin-build > /dev/null; then
+        echo "darwin-build is already installed on this system."
+    else
+        export NIX_PATH=darwin=darwin-config=$BINDIR/../modules/darwin/default.nix:$HOME/.nix-defexpr/channels:$NIX_PATH
+        $(nix-build '<darwin>' -A system --no-out-link)/sw/bin/darwin-rebuild build
+        $(nix-build '<darwin>' -A system --no-out-link)/sw/bin/darwin-rebuild switch
+    fi
+fi

9c81a88c939cd4151413765b5d820d4c1bd481b3
Ron Parker <rdparker@gmail.com>
2022-05-13 10:50:02 -0400

    Add a Raycast script for the scratch Emacs profile

---
+++ modules/home-manager/dotfiles/raycast/scratch-emacs.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Required parameters:
+# @raycast.schemaVersion 1
+# @raycast.title Doom Emacs
+# @raycast.mode silent
+
+# Optional parameters:
+# @raycast.icon 🍳
+# @raycast.packageName Doom Emacs
+
+open "/Users/pron/Applications/Nix Apps/Emacs.app" --args --with-profile scratch
+
